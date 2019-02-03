f62fd67f910be073daeb9932ec83fa0eacda7d1a
codeforges <ruben@codeforges.com>
2019-02-03 09:40:48 +0100

    Death animations

--- Assets/Scripts/Entities/MainGun.cs
+++ Assets/Scripts/Entities/MainGun.cs
@@ -14,46 +14,64 @@ public class MainGun : MonoBehaviour
     public int health = 10;
     public int maxDamageAnimationClips = 3;
     public GameObject healthBar;
+    public ParticleSystem deathParticles;
     
     private int damageAnimationCounter = 1;
+    private Animator turretAnimator;
+    private Animator gunAnimator;
+    private bool _isDead;
     
-    
+    private void Start()
+    {
+        turretAnimator = transform.parent.GetComponent<Animator>();
+        gunAnimator = GetComponent<Animator>();
+    }
+
     public void takeDamage(int amount)
     {
         health -= amount;
 
         if (damageAnimationCounter <= maxDamageAnimationClips)
         {
-            var animator = transform.parent.GetComponent<Animator>();
-            animator.SetInteger("DamageCounter", damageAnimationCounter);
-            animator.Play("TurretHit");
+            turretAnimator.SetInteger("DamageCounter", damageAnimationCounter);
+            turretAnimator.Play("TurretHit");
             Destroy(healthBar.transform.GetChild(0).gameObject);
             damageAnimationCounter++;
         }
         
         if (health <= 0)
         {
+            StartCoroutine(Destroy());
             Debug.Log("GameOver");
         }
+    }
+    
+    public bool isDead
+    {
+        get { return health <= 0; }
     }
     
     // Update is called once per frame
     void Update()
     {
-        TargetMouse();
-        if (Input.GetButtonDown("Fire1"))
+        if (!isDead)
         {
-            var gunPosition = gunShootPoint.transform.position;
+            TargetMouse();
+            if (Input.GetButtonDown("Fire1"))
+            {
+                var gunPosition = gunShootPoint.transform.position;
             
-            var muzzle = Instantiate(muzzleFlash);
-            muzzle.transform.position = gunPosition;
+                var muzzle = Instantiate(muzzleFlash);
+                muzzle.transform.position = gunPosition;
             
-            var light = Instantiate(muzzleLight);
-            light.transform.position = new Vector3(gunPosition.x, gunPosition.y, -1.3f);
+                var light = Instantiate(muzzleLight);
+                light.transform.position = new Vector3(gunPosition.x, gunPosition.y, -1.3f);
 
+            }
+
+            muzzleFlash.transform.localRotation = transform.rotation;
         }
-
-        muzzleFlash.transform.localRotation = transform.rotation;
+       
     }
 
     private void TargetMouse()
@@ -64,5 +82,11 @@ public class MainGun : MonoBehaviour
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
     }
 
-    
+    private IEnumerator Destroy()
+    {
+        Instantiate(deathParticles, transform);
+        turretAnimator.SetTrigger("Destroyed");
+        gunAnimator.SetTrigger("Destroyed");
+        yield return new WaitForSeconds(turretAnimator.GetCurrentAnimatorStateInfo(0).length);
+    }
 }
